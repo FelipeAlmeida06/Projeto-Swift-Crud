7032e981ed36cfe041cfe6f97dd774a395f8214d
Felipe Antônio de O. Almeida <oliveira.lipe.2006@gmail.com>
2024-11-05 10:43:24 -0300

    create new project swift firebase

---
+++ Projeto-Swift-Crud/Projeto-Swift-Crud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projeto-Swift-Crud
+//
+//  Created by FELIPE on 05/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projeto-Swift-Crud/Projeto-Swift-Crud/Projeto_Swift_CrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projeto_Swift_CrudApp.swift
+//  Projeto-Swift-Crud
+//
+//  Created by FELIPE on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Projeto_Swift_CrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
