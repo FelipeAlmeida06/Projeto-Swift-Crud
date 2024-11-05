49b57045f8c817d244fe95eecd2a13dbdd17a036
Felipe Antônio de O. Almeida <oliveira.lipe.2006@gmail.com>
2024-11-05 12:08:07 -0300

    criando a tela de cadastro

--- Projeto-Swift-Crud/Projeto-Swift-Crud/View/CadastroView.swift
+++ Projeto-Swift-Crud/Projeto-Swift-Crud/View/CadastroView.swift
@@ -27,16 +27,63 @@ struct CadastroView: View {
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Informaçoes do Filme")) {
+                //Informaçoes do Filme
+                Section(header: Text("Insira as Informacoes")) {
                     TextField("Nome", text: $nomeFilme)
                     TextField("Nome do Diretor", text: $nomeDiretor)
                     TextField("Sinopse", text: $sinopseFilme)
                     TextField("Ano de Lancamento", text: $anoLancamento)
                     TextField("Url da Imagem", text: $urlImagem)
                 }
+                
+                Button(action: {
+                    salvarNoFirebase()
+                }) {
+                    Text("Salvar Filme")
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
             }
+            .navigationTitle("Cadastro de Filme")
+            .padding()
         }
     }
+    
+    // salvarFilme()
+    func salvarNoFirebase() {
+            // Verifica se todos os campos estão preenchidos
+            guard !nomeFilme.isEmpty, !nomeDiretor.isEmpty, !sinopseFilme.isEmpty, !anoLancamento.isEmpty, !urlImagem.isEmpty else {
+                print("Por favor, preencha todos os campos.")
+                return
+            }
+            
+            // Cria um dicionário com os dados do filme
+            let filmeData: [String: Any] = [
+                "nomeFilme": nomeFilme,
+                "nomeDiretor": nomeDiretor,
+                "sinopse": sinopseFilme,
+                "anoLancamento": anoLancamento,
+                "urlImagem": urlImagem
+            ]
+            
+            // Salva os dados no Firestore
+            db.collection("filmes").addDocument(data: filmeData) { error in
+                if let error = error {
+                    print("Erro ao salvar filme: \(error)")
+                } else {
+                    print("Filme salvo com sucesso!")
+                    // Limpa os campos após salvar
+                    nomeFilme = ""
+                    nomeDiretor = ""
+                    sinopseFilme = ""
+                    anoLancamento = ""
+                    urlImagem = ""
+                }
+            }
+        }
 }
 
 #Preview {
